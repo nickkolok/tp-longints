a8a502f7e3cf40d0c568324969038325c0825348
NickKolok <nickkolok@mail.ru>
2018-05-11 12:04:49 +0300

    lr5: drafts of multiplication in a ring, CLI

---
+++ ring_mult.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+#include "class_int_ring.cpp"
+
+int main(){
+
+	cout << "Введите модуль кольца вычетов:" << endl;
+	BigInt modulo(cin, 4);
+
+	cout << "Введите первый операнд:" << endl;
+	BigInt b1(cin, 4);
+	cout << "Введитe второй операнд:" << endl;
+	BigInt b2(cin, 4);
+	cout << "Введите знак операции:  *  или  ^" << endl;
+	char act;
+	cin >> act;
+
+	BigInt b3(4);
+
+	switch(act){
+		case '*':
+			b3 = multiplyRingDirect(b1, b2, modulo);
+		break;
+		case '^':
+			//b3 = subtractRing(b1, b2, modulo);
+		break;
+		default:
+			cout << "Некорректная операция" << endl;
+			return -2;
+	}
+	cout << b3 << endl;
+	return 0;
+}
