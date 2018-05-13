7560de54a84cdf30c32e4a37ab923b4a20e7e765
NickKolok <nickkolok@mail.ru>
2018-05-13 18:52:45 +0300

    One redundant function is removed

--- class_int_factor.cpp
+++ class_int_factor.cpp
@@ -36,32 +36,6 @@ BigInt gcdNoCoeff(BigInt x, BigInt y){
 	return y;
 }
 
-BigInt findDivisor(BigInt dividend, BigInt upTo){
-	if(isEven(dividend)){
-		return BigInt(dividend.m_nExp, 1, 2);
-	}
-	BigInt rem(dividend.m_nExp);
-	divideByDigit(dividend, 3, &rem);
-	if(rem[0]==0){
-		return BigInt(dividend.m_nExp, 1, 3);
-	}
-
-	BigInt the1(dividend.m_nExp, 1, 1);
-	BigInt probe = BigInt(dividend.m_nExp, 1, 3);
-	while(compareAbs(upTo, probe)>0){
-		probe.addDigit(2);
-		BigInt companion = probe;
-		probe.addDigit(2);
-		companion = multiply(companion, probe);
-		probe.addDigit(2);
-		BigInt gcd = gcdNoCoeff(companion, dividend);
-		if(compareAbs(gcd, the1)>0){
-			return gcd;
-		}
-	}
-	return dividend;
-}
-
 ostream& operator << (ostream& sout, Factors f){
 	int s = f.size() - 1;
 	for(int i = 0; i < s; i++){
